874f4bbe75bc1945617f02fa23bd4dbded2844b7
Paulo Ferreira <phgferreira@outlook.com>
2021-03-13 17:44:44 -0300

    Start with Spring Boot

---
+++ src/main/java/org/hst/factoryofideas/FactoryofideasApplication.java
@@ -0,0 +1,13 @@
+package org.hst.factoryofideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryofideasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryofideasApplication.class, args);
+	}
+
+}
